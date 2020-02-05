4e71802496eb80d6ca2c46f6caf876b4aa255e25
moeti <moetichentuf@hotmail.com>
2020-02-05 11:31:49 +0100

    trying out api

---
+++ README.md
@@ -0,0 +1,1 @@
+#### **The pokemon challenge - PHP style**
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-//index.php
-echo 'dit is fout';
+$contents = file_get_contents('https://pokeapi.co/api/v2/pokemon/ditto/');
+$contents = utf8_encode($contents);
+$results = json_decode($contents);
+
+echo "<pre>";
+var_dump($results);
+echo "</pre>";
+
