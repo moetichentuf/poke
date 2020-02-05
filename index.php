5bc0f896d01c97684f82d20f3e53c6ec2a0513d8
moeti <moetichentuf@hotmail.com>
2020-02-05 15:15:58 +0100

    able to look up pokemon with number

--- index.php
+++ index.php
@@ -1,9 +1,38 @@
+
 <?php
-$contents = file_get_contents('https://pokeapi.co/api/v2/pokemon/ditto/');
-$contents = utf8_encode($contents);
-$results = json_decode($contents);
+// variable with Poke API and value from input
+$url = 'https://pokeapi.co/api/v2/pokemon/' . $_GET['value']; // Pokemon API url + search
+$poke_json = file_get_contents($url); // to get data from API
+$result_array = json_decode($poke_json , true); // 'true' converts objects into associative arrays
 
-echo "<pre>";
-var_dump($results);
-echo "</pre>";
 
+// Pokemon variable
+$name = $result_array['name'];
+
+// Display
+var_dump($name);
+
+?>
+
+
+
+
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form name="form" action="" method="get"   >
+    <input type="text" name="value">
+    <button type="submit" ></button
+</form>
+</body>
+</html>
